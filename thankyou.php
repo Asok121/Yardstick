2a459998f64756a32e141c4109835cb5ef50753f
Asok121 <131285734+Asok121@users.noreply.github.com>
2024-01-24 21:22:19 +0530

    Add files via upload

---
+++ thankyou.php
@@ -0,0 +1,51 @@
+<?php include("header.php")?>
+<!-- Meta Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '933293495030725');
+fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=933293495030725&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Meta Pixel Code -->
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-ZZ6J693755"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-ZZ6J693755');
+</script>
+<section class="bg-gradient pad-tb" style="margin-top: 50px;">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 text-center">
+				<div class="text-center pt-5 pb-5">
+				  <h1 class="display-3 cus-color">Thank You!</h1>
+				  <p class="lead" style="text-align: center;">We've received your message and will get back with a response in a day.</p>
+				  <hr>
+				  <!--<p>Having trouble? <a href="">Contact us</a></p>-->
+				  <p class="lead pt-5" style="text-align: center;">
+					<a href="index.php" class="btn-outline">Back to Home</a>
+				  </p>
+				</div>
+				
+			</div>
+		</div>
+	</div>
+</section>
+<style>
+	.cus-color{
+		color: #000;
+	}
+</style>
+<?php include("footer.php")?>
